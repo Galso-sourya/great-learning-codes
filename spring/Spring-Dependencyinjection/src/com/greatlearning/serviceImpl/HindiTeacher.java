a8d55346a00f10a984600bdb10e44667e65da956
Galso-sourya <75131149+Galso-sourya@users.noreply.github.com>
2024-03-12 22:14:51 +0530

    Update HindiTeacher.java
    what is the function of default constructor?

--- spring/Spring-Dependencyinjection/src/com/greatlearning/serviceImpl/HindiTeacher.java
+++ spring/Spring-Dependencyinjection/src/com/greatlearning/serviceImpl/HindiTeacher.java
@@ -7,7 +7,7 @@ public class HindiTeacher implements Teacher {
 //defining a private field for dependency
 	ExamTip examTip;
 	public HindiTeacher() {//this is the default constructor
-		
+//this constructor will initialize the private field examTip to null		
 	}
 	//define a constructor for dependency injection. this is a parameterized constructor
 	public HindiTeacher(ExamTip theExamTip) {
