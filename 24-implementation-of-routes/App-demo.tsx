7f4aa76c0ce4ff8146541ced848034891762a6aa
Galso-sourya <75131149+Galso-sourya@users.noreply.github.com>
2024-12-21 12:36:25 +0600

    Update App-demo.tsx

--- 24-implementation-of-routes/App-demo.tsx
+++ 24-implementation-of-routes/App-demo.tsx
@@ -1,5 +1,8 @@
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom';
+/*we have to also run the below command line---
+
+*/
 export default(
     <BrowserRouter>
  <App/>   
