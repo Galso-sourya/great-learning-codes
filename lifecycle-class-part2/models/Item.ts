bfe4bc3b41cbdf9aa2410d43330b156a8abf4bdc
Galso-sourya <75131149+Galso-sourya@users.noreply.github.com>
2025-04-15 17:07:58 +0600

    Add files via upload

---
+++ lifecycle-class-part2/models/Item.ts
@@ -0,0 +1,6 @@
+type Item={//we will make a component which will fetch the item dynamically and show content according
+    //to the page number
+    id:number,
+    name:string
+};
+export default Item;
