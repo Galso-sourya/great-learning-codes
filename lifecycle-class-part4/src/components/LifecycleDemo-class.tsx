c443b95a79d8fa86daae4432bbea3a422cf7201f
Galso-sourya <75131149+Galso-sourya@users.noreply.github.com>
2025-04-26 00:09:24 +0600

    Add files via upload

---
+++ lifecycle-class-part4/src/components/LifecycleDemo-class.tsx
@@ -0,0 +1,77 @@
+import React,{Component} from 'react';//we are importing component as it is a class component
+import Item from '../../models/Item';//as item is the default export, we should not keep that in braces
+import { fetchProductsOfPage } from '../services/products';
+type Props={};
+type State={
+page:number,
+status: 'LOADING' | 'LOADED' | 'ERROR',
+products:Item[],
+error: Error| null//either error object or null
+}
+class LifecycleDemo extends Component<Props,State>{
+    constructor(props:Props){
+        //console.log('constructor');
+        super(props);
+        this.state={//this is the initial point of the state
+            page:1,
+            status:'LOADING',
+            products:[],
+            error:null
+                };
+    }
+    nextPage=()=>{
+        this.setState(
+            state=>{
+                return{
+page:state.page+1
+                };
+            }
+        );
+    }
+render(){
+    const {page,status,products,error}=this.state;//destructuring step
+    return(
+        <>
+        {status==='LOADING' && <div>loading products...</div>}
+        {status==='ERROR' && <div>{error?.message}</div>}
+        {
+        status==='LOADED' && (
+            <>
+            <button onClick={this.nextPage}>next page</button>
+            <span>you are on page{page}</span>
+            <ul>
+                {
+                    products.map(
+                        ({id,name})=><li key={id}>{name}</li>
+                    )
+                }
+            </ul>
+            </>
+        )
+        
+        }
+        </>
+    )}
+    async fetchProductsOfPage(){
+        this.setState({
+            status:'LOADING'
+        });
+const products=await fetchProductsOfPage(this.state.page);
+this.setState({
+status:'LOADED',
+products//after loaded, the products will remain same. that is the meaning of this line. it is equal 
+//to the initial value 
+});
+    }
+    componentDidMount(){//this is ajax call
+this.fetchProductsOfPage();
+    }
+    componentDidUpdate(oldProps:Props,oldState:State){
+        if(oldState.page!==this.state.page){//we do not run the function all the time, only when 
+            //the page state changes
+this.fetchProductsOfPage();
+}
+    }   
+    
+}
+export default LifecycleDemo;
